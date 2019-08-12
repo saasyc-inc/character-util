be8e175c2f4740cfe83723c16f0f681439be6bc7
杨益民 <744541711@qq.com>
2019-08-12 17:20:22 +0800

    添加过滤不可见字符

--- src/UnVisibleCharacterFilter.php
+++ src/UnVisibleCharacterFilter.php
@@ -12,6 +12,7 @@ class UnVisibleCharacterFilter
 {
     public static function main($info)
     {
+        //只返回　utf-8 当中的可见字符
         return preg_replace('/[\x00-\x1F\x7F]/u', '', $info);
     }
 }
